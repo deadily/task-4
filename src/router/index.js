66db861db0ffeb642baf853d7a0a284d21ba64c5
dara <zuevadara04@gmail.com>
2026-03-11 00:27:12 +0700

    did base from documentation

--- src/router/index.js
+++ src/router/index.js
@@ -6,3 +6,38 @@ const router = createRouter({
 })
 
 export default router
+
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.getters.isAuthenticated) {
+    next();
+    return;
+  }
+  next('/');
+};
+
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters.isAuthenticated) {
+    next();
+    return;
+  }
+  next('/login');
+};
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: function () {
+      return import('../views/HomeView.vue');
+    },
+    beforeEnter: ifAuthenticated,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: function () {
+      return import('../components/Login.vue');
+    },
+    beforeEnter: ifNotAuthenticated,
+  }
+]
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { createStore } from 'vuex';
+import { loginRequest } from '@/utils/api.js';
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
@@ -10,3 +12,37 @@ export const useCounterStore = defineStore('counter', () => {
 
   return { count, doubleCount, increment }
 })
+
+export default createStore({
+  state: {
+    token: localStorage.getItem('myActivationToken') || '',
+  },
+  getters: {
+    isAuthenticated: (state) => !!state.token,
+  },
+  actions: {
+    AUTH_REQUEST: ({ commit }, user) => {
+      return new Promise((resolve, reject) => {
+        loginRequest(user)
+          .then((token) => {
+            commit('AUTH_SUCCESS', token);
+            localStorage.setItem('myActivationToken', token);
+            resolve();
+          })
+          .catch(() => {
+            commit('AUTH_ERROR');
+            localStorage.removeItem('myActivationToken');
+            reject();
+          });
+      });
+    },
+  },
+  mutations: {
+    AUTH_SUCCESS: (state, token) => {
+      state.token = token;
+    },
+    AUTH_ERROR: (state) => {
+      state.token = '';
+    },
+  },
+});
